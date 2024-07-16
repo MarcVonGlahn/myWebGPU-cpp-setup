1eeaafc207b566a47168ad0a51301aaa4f167a29
Marc von Glahn <marc.von.glahn@gmail.com>
2024-07-16 14:06:41 +0200

    My implementation of hello triangle

--- main.cpp
+++ main.cpp
@@ -16,6 +16,30 @@
 // Avoid the "wgpu::" prefix in front of all WebGPU symbols
 using namespace wgpu;
 
+
+// Global Declarations
+const char* shaderSource = R"(
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4f {
+    var p = vec2f(0.0, 0.0);
+    if (in_vertex_index == 0u) {
+        p = vec2f(-0.5, -0.5);
+    } else if (in_vertex_index == 1u) {
+        p = vec2f(0.5, -0.5);
+    } else {
+        p = vec2f(0.0, 0.5);
+    }
+    return vec4f(p, 0.0, 1.0);
+}
+
+@fragment
+fn fs_main() -> @location(0) vec4f {
+    return vec4f(0.0, 0.4, 1.0, 1.0);
+}
+)";
+
+
+
 class Application {
 public:
 	// Initialize everything and return true if it went all right
@@ -33,6 +57,8 @@ public:
 private:
 	TextureView GetNextSurfaceTextureView();
 
+	void InitializePipeline();
+
 private:
 	// We put here all the variables that are shared between init and main loop
 	GLFWwindow *window;
@@ -40,6 +66,9 @@ private:
 	Queue queue;
 	Surface surface;
 	std::unique_ptr<ErrorCallback> uncapturedErrorCallbackHandle;
+
+	RenderPipeline pipeline;
+	TextureFormat surfaceFormat = TextureFormat::Undefined;
 };
 
 int main() {
@@ -115,7 +144,7 @@ bool Application::Initialize() {
 	config.width = 640;
 	config.height = 480;
 	config.usage = TextureUsage::RenderAttachment;
-	TextureFormat surfaceFormat = surface.getPreferredFormat(adapter);
+	surfaceFormat = surface.getPreferredFormat(adapter);
 	config.format = surfaceFormat;
 
 	// And we do not need any particular view format:
@@ -129,11 +158,15 @@ bool Application::Initialize() {
 
 	// Release the adapter only after it has been fully utilized
 	adapter.release();
+
+	// At the end of Initialize()
+	InitializePipeline();
 	
 	return true;
 }
 
 void Application::Terminate() {
+	pipeline.release();
 	surface.unconfigure();
 	queue.release();
 	surface.release();
@@ -154,6 +187,7 @@ void Application::MainLoop() {
 	encoderDesc.label = "My command encoder";
 	CommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoderDesc);
 
+
 	// Create the render pass that clears the screen with our color
 	RenderPassDescriptor renderPassDesc = {};
 
@@ -163,7 +197,7 @@ void Application::MainLoop() {
 	renderPassColorAttachment.resolveTarget = nullptr;
 	renderPassColorAttachment.loadOp = LoadOp::Clear;
 	renderPassColorAttachment.storeOp = StoreOp::Store;
-	renderPassColorAttachment.clearValue = WGPUColor{ 0.9, 0.1, 0.2, 1.0 };
+	renderPassColorAttachment.clearValue = WGPUColor{ 0.8, 0.2, 0.2, 1.0 };
 #ifndef WEBGPU_BACKEND_WGPU
 	renderPassColorAttachment.depthSlice = WGPU_DEPTH_SLICE_UNDEFINED;
 #endif // NOT WEBGPU_BACKEND_WGPU
@@ -175,6 +209,12 @@ void Application::MainLoop() {
 
 	// Create the render pass and end it immediately (we only clear the screen but do not draw anything)
 	RenderPassEncoder renderPass = encoder.beginRenderPass(renderPassDesc);
+
+	// Select which render pipeline to use
+	renderPass.setPipeline(pipeline);
+	// Draw 1 instance of a 3-vertices shape
+	renderPass.draw(3, 1, 0, 0);
+
 	renderPass.end();
 	renderPass.release();
 
@@ -228,4 +268,110 @@ TextureView Application::GetNextSurfaceTextureView() {
 	TextureView targetView = texture.createView(viewDescriptor);
 
 	return targetView;
+}
+
+void Application::InitializePipeline()
+{
+	ShaderModuleDescriptor shaderDesc;
+	// [...] Describe shader module
+	ShaderModule shaderModule = device.createShaderModule(shaderDesc);
+	ShaderModuleWGSLDescriptor shaderCodeDesc;
+	// Set the chained struct's header
+	shaderCodeDesc.chain.next = nullptr;
+	shaderCodeDesc.chain.sType = SType::ShaderModuleWGSLDescriptor;
+	// Connect the chain
+	shaderDesc.nextInChain = &shaderCodeDesc.chain;
+
+	shaderCodeDesc.code = shaderSource;
+
+	RenderPipelineDescriptor pipelineDesc;
+
+	// [...] Describe vertex pipeline state
+	// Configure 'pipelineDesc.vertex'
+	// [...] Describe vertex buffers
+	// We do not use any vertex buffer for this first simplistic example
+	pipelineDesc.vertex.bufferCount = 0;
+	pipelineDesc.vertex.buffers = nullptr;
+	// [...] Describe vertex shader
+	// NB: We define the 'shaderModule' in the second part of this chapter.
+	// Here we tell that the programmable vertex shader stage is described
+	// by the function called 'vs_main' in that module.
+	pipelineDesc.vertex.module = shaderModule;
+	pipelineDesc.vertex.entryPoint = "vs_main";
+	pipelineDesc.vertex.constantCount = 0;
+	pipelineDesc.vertex.constants = nullptr;
+	
+	// [...] Describe primitive pipeline state
+	// Each sequence of 3 vertices is considered as a triangle
+	pipelineDesc.primitive.topology = PrimitiveTopology::TriangleList;
+
+	// We'll see later how to specify the order in which vertices should be
+	// connected. When not specified, vertices are considered sequentially.
+	pipelineDesc.primitive.stripIndexFormat = IndexFormat::Undefined;
+
+	// The face orientation is defined by assuming that when looking
+	// from the front of the face, its corner vertices are enumerated
+	// in the counter-clockwise (CCW) order.
+	pipelineDesc.primitive.frontFace = FrontFace::CCW;
+
+	// But the face orientation does not matter much because we do not
+	// cull (i.e. "hide") the faces pointing away from us (which is often
+	// used for optimization).
+	pipelineDesc.primitive.cullMode = CullMode::None;
+
+	
+	// [...] Describe fragment pipeline state
+	// We tell that the programmable fragment shader stage is described
+	// by the function called 'fs_main' in the shader module.
+	FragmentState fragmentState;
+	fragmentState.module = shaderModule;
+	fragmentState.entryPoint = "fs_main";
+	fragmentState.constantCount = 0;
+	fragmentState.constants = nullptr;
+
+	// [...] We'll configure the blending stage here
+	BlendState blendState;
+	// [...] Configure color blending equation
+	blendState.color.srcFactor = BlendFactor::SrcAlpha;
+	blendState.color.dstFactor = BlendFactor::OneMinusSrcAlpha;
+	blendState.color.operation = BlendOperation::Add;
+	// [...] Configure alpha blending equation
+	blendState.alpha.srcFactor = BlendFactor::Zero;
+	blendState.alpha.dstFactor = BlendFactor::One;
+	blendState.alpha.operation = BlendOperation::Add;
+
+	ColorTargetState colorTarget;
+	colorTarget.format = surfaceFormat;
+	colorTarget.blend = &blendState;
+	colorTarget.writeMask = ColorWriteMask::All; // We could write to only some of the color channels.
+
+	// We have only one target because our render pass has only one output color
+	// attachment.
+	fragmentState.targetCount = 1;
+	fragmentState.targets = &colorTarget;
+
+	pipelineDesc.fragment = &fragmentState;
+
+
+
+	// [...] Describe stencil/depth pipeline state
+	// We do not use stencil/depth testing for now
+	pipelineDesc.depthStencil = nullptr;
+
+	// [...] Describe multi-sampling state
+	// Samples per pixel
+	pipelineDesc.multisample.count = 1;
+	// Default value for the mask, meaning "all bits on"
+	pipelineDesc.multisample.mask = ~0u;
+	// Default value as well (irrelevant for count = 1 anyways)
+	pipelineDesc.multisample.alphaToCoverageEnabled = false;
+
+	// [...] Describe pipeline layout
+	pipelineDesc.layout = nullptr;
+
+
+	pipeline = device.createRenderPipeline(pipelineDesc);
+
+	// We no longer need to access the shader module
+	shaderModule.release();
 }
